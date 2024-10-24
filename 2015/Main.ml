edd6aa32612e846080dab8ed7f2b02dd9d5547ae
Christopher Kaster <me@atomicptr.de>
2024-10-25 01:36:41 +0200

    move data into its own file for day 1, add day 2

--- 2015/Main.ml
+++ 2015/Main.ml
@@ -1,4 +1,5 @@
 let () = match Base.get_day() with
-| Some 1 -> Day01.run ()
+| Some 1 -> Day01.run (Base.read_text "./Day01_input.txt")
+| Some 2 -> Day02.run (Base.read_text "./Day02_input_part1.txt")
 | Some n -> print_endline ("Unknown day specified: " ^ (string_of_int n))
 | None -> print_endline "Could not determine which day to run"
--- base/Base.ml
+++ base/Base.ml
@@ -3,3 +3,12 @@ let args = List.tl @@ Array.to_list Sys.argv
 let get_day () = match args with
 | day :: _ -> Some (int_of_string day)
 | _ -> None
+
+(* From atomicptr/pathlib-ml *)
+
+(** Read text from file *)
+let read_text file =
+  let in_channel = open_in_bin file in
+  let str = really_input_string in_channel (in_channel_length in_channel) in
+  close_in in_channel;
+  str
