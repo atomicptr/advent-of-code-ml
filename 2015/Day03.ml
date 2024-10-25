d4cb065ae9e364c392ede5ca0075b82f967a3131
Christopher Kaster <me@atomicptr.de>
2024-10-25 15:35:41 +0900

    add solution for part 2 of day 3

--- 2015/Day03.ml
+++ 2015/Day03.ml
@@ -11,11 +11,11 @@ module CoordMap = struct
       | res -> res
   end)
 
-  let init_pos key map =
+  let init_pos key map value =
     update key
       (function
         | Some value -> Some value
-        | None -> Some 1)
+        | None -> Some value)
       map
 
   let increment key map =
@@ -24,6 +24,15 @@ module CoordMap = struct
         | Some value -> Some (value + 1)
         | None -> Some 1)
       map
+
+  let merge_values map1 map2 =
+    merge
+      (fun _ v1 v2 ->
+        match (v1, v2) with
+        | Some v1, Some v2 -> Some (v1 + v2)
+        | Some v, None | None, Some v -> Some v
+        | None, None -> None)
+      map1 map2
 end
 
 let char_to_direction = function
@@ -35,7 +44,7 @@ let char_to_direction = function
 
 let deliver_present direction current visited =
   let x, y = current in
-  let visited = CoordMap.init_pos current visited in
+  let visited = CoordMap.init_pos current visited 1 in
   match direction with
   | North ->
       let new_pos = (x, y - 1) in
@@ -57,6 +66,17 @@ let rec deliver_present_path directions current visited =
       let visited, new_pos = deliver_present dir current visited in
       deliver_present_path rest new_pos visited
 
+let rec deliver_present_path_with_robot directions (current_santa, visited_santa) (current_robo, visited_robo) =
+  match directions with
+  | [] -> (visited_santa, visited_robo)
+  | [ dir ] ->
+      let visited_santa, new_pos = deliver_present dir current_santa visited_santa in
+      deliver_present_path_with_robot [] (new_pos, visited_santa) (current_robo, visited_robo)
+  | dir_santa :: dir_robot :: rest ->
+      let visited_santa, santa_new_pos = deliver_present dir_santa current_santa visited_santa in
+      let visisted_robo, robo_new_pos = deliver_present dir_robot current_robo visited_robo in
+      deliver_present_path_with_robot rest (santa_new_pos, visited_santa) (robo_new_pos, visisted_robo)
+
 let parse str =
   List.init (String.length str) (String.get str)
   |> List.map char_to_direction
@@ -64,10 +84,17 @@ let parse str =
   |> List.map Option.get
 
 let run input =
-  print_endline
+  (print_endline
   @@ "The result of part 1 is: "
   ^ string_of_int
   @@ CoordMap.cardinal
   @@
   let visited, _ = deliver_present_path (parse input) (0, 0) CoordMap.empty in
-  visited
+  visited);
+  print_endline
+  @@ "The result of part 2 is: "
+  ^ string_of_int
+  @@ CoordMap.cardinal
+  @@
+  let santa, robot = deliver_present_path_with_robot (parse input) ((0, 0), CoordMap.empty) ((0, 0), CoordMap.empty) in
+  CoordMap.merge_values santa robot
