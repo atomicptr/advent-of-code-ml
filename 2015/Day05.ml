3638d471ab2232daaaa9050d3119b8539157e171
Christopher Kaster <me@atomicptr.de>
2024-11-03 11:18:58 +0900

    add day 05 part 2

--- 2015/Day05.ml
+++ 2015/Day05.ml
@@ -31,5 +31,38 @@ let satisfies_rule_3 str =
   test (Base.explode str)
 
 let is_nice str = satisfies_rule_1 str && satisfies_rule_2 str && satisfies_rule_3 str
-let parse_file str = Base.split_on_newline str |> List.filter is_nice |> List.length
-let run input = print_endline @@ "Part 1: " ^ string_of_int (parse_file input)
+
+module StringMap = Map.Make (String)
+
+let satisfies_new_rule_1 str =
+  let increment map key =
+    StringMap.update key
+      (function
+        | Some value -> Some (value + 1)
+        | None -> Some 1)
+      map
+  in
+  let rec parse lst map =
+    match lst with
+    | [] | [ _ ] -> map
+    | a :: b :: c :: d :: rest when a == b && b == c && c == d ->
+        parse (b :: c :: d :: rest) (increment map (String.make 1 a ^ String.make 1 b))
+    | a :: b :: c :: rest when a == b && b == c -> parse (b :: c :: rest) map
+    | a :: b :: rest -> parse (b :: rest) (increment map (String.make 1 a ^ String.make 1 b))
+  in
+  let result = List.filter (fun (_, res) -> res >= 2) (StringMap.to_list @@ parse (Base.explode str) StringMap.empty) in
+  List.length result > 0
+
+let satisfies_new_rule_2 str =
+  let rec test = function
+    | [] | [ _ ] | [ _; _ ] -> false
+    | a :: b :: c :: rest -> if a == c then true else test @@ (b :: c :: rest)
+  in
+  test (Base.explode str)
+
+let is_nice_part_2 str = satisfies_new_rule_1 str && satisfies_new_rule_2 str
+let parse_file str func = Base.split_on_newline str |> List.filter func |> List.length
+
+let run input =
+  print_endline @@ "Part 1: " ^ string_of_int (parse_file input is_nice);
+  print_endline @@ "Part 2: " ^ string_of_int (parse_file input is_nice_part_2)
--- 2015/Day05_test.ml
+++ 2015/Day05_test.ml
@@ -18,6 +18,18 @@ let test () =
   check_ok Day05.is_nice "aaa" true;
   check_ok Day05.is_nice "jchzalrnumimnmhp" false;
   check_ok Day05.is_nice "haegwjzuvuyypxyu" false;
-  check_ok Day05.is_nice "dvszwmarrgswjxmb" false
+  check_ok Day05.is_nice "dvszwmarrgswjxmb" false;
+  check_ok Day05.satisfies_new_rule_1 "xyxy" true;
+  check_ok Day05.satisfies_new_rule_1 "aabcdefgaa" true;
+  check_ok Day05.satisfies_new_rule_1 "aaa" false;
+  check_ok Day05.satisfies_new_rule_2 "xyx" true;
+  check_ok Day05.satisfies_new_rule_2 "abcdefeghi" true;
+  check_ok Day05.satisfies_new_rule_2 "efe" true;
+  check_ok Day05.satisfies_new_rule_2 "abc" false;
+  check_ok Day05.is_nice_part_2 "qjhvhtzxzqqjkmpb" true;
+  check_ok Day05.is_nice_part_2 "xxyxx" true;
+  check_ok Day05.is_nice_part_2 "uurcxstgmygtbstg" false;
+  check_ok Day05.is_nice_part_2 "ieodomkazucvgmuy" false;
+  check_ok Day05.is_nice_part_2 "aaaa" true
 
 let () = Alcotest.run "2015-05" [ ("Day 05", [ ("can run day 05 examples", `Quick, test) ]) ]
