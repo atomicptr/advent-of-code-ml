716ed19c88f3ea92e4cc9d8571eaa6e6c4348d9e
Christopher Kaster <me@atomicptr.de>
2024-11-16 11:48:02 +0100

    increase performance of day 2015-06 by switching to array

--- 2015/Day06.ml
+++ 2015/Day06.ml
@@ -16,27 +16,27 @@ let parse str =
       Some (Toggle (parse_coord start_str |> Option.get, parse_coord end_str |> Option.get))
   | _ -> None
 
-type grid = bool list * int
+type grid = bool array * int
 
 let make_grid_opt width height =
   match (width, height) with
   | width, height when width <= 0 || height <= 0 -> None
-  | width, height -> Some (List.init (width * height) (fun _ -> false), width)
+  | width, height -> Some (Array.init (width * height) (fun _ -> false), width)
 
 let make_grid width height = make_grid_opt width height |> Option.get
 let grid_index (_, width) x y = (y * width) + x
 
 let grid_get_opt (grid, width) x y =
   match (x, y) with
-  | x, y when x < 0 || x > width || y < 0 || y > List.length grid / width -> None
-  | x, y -> Some (List.nth grid (grid_index (grid, width) x y))
+  | x, y when x < 0 || x > width || y < 0 || y > Array.length grid / width -> None
+  | x, y -> Some grid.(grid_index (grid, width) x y)
 
 let grid_get grid x y = grid_get_opt grid x y |> Option.get
 
 let grid_set (grid, width) x y value =
   match (x, y) with
-  | x, y when x < 0 || x > width || y < 0 || y > List.length grid / width -> (grid, width)
-  | x, y -> (List.mapi (fun index v -> if index == (y * width) + x then value else v) grid, width)
+  | x, y when x < 0 || x > width || y < 0 || y > Array.length grid / width -> (grid, width)
+  | x, y -> (Array.mapi (fun index v -> if index == (y * width) + x then value else v) grid, width)
 
 let grid_apply (grid, width) (from_x, from_y) (to_x, to_y) func =
   let iter index v =
@@ -48,11 +48,11 @@ let grid_apply (grid, width) (from_x, from_y) (to_x, to_y) func =
     | x, y when x >= from_x && y >= from_y -> func v
     | _ -> v
   in
-  (List.mapi iter grid, width)
+  (Array.mapi iter grid, width)
 
 let grid_set_range grid from_coord to_coord value = grid_apply grid from_coord to_coord (fun _ -> value)
 let grid_flip_range grid from_coord to_coord = grid_apply grid from_coord to_coord (fun value -> Bool.not value)
-let grid_count (grid, _) = List.fold_left (fun acc curr -> if curr then acc + 1 else acc) 0 grid
+let grid_count (grid, _) = Array.fold_left (fun acc curr -> if curr then acc + 1 else acc) 0 grid
 
 let eval grid = function
   | TurnOn (from_coord, to_coord) -> grid_set_range grid from_coord to_coord true
